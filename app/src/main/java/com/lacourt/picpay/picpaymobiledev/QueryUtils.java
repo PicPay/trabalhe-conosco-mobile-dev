411c2dea7eaeb9bb297c1970fb2aabdb7f472149
igor lacourt <igorlacourt@gmail.com>
2017-08-25 09:25:51 -0300

    Teste Commit.

---
+++ app/src/main/java/com/lacourt/picpay/picpaymobiledev/QueryUtils.java
@@ -0,0 +1,135 @@
+package com.lacourt.picpay.picpaymobiledev;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class QueryUtils {
+
+    private QueryUtils(){}
+
+    private final static String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    public static ArrayList<User> fetchEarthquakeData(String requestUrl) {
+
+        ArrayList<User> users;
+
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(QueryUtils.class.getSimpleName(), "Error closing input stream", e);
+        }
+
+        try{
+//            JSONObject jsonObject = new JSONObject(jsonResponse);
+            JSONArray jsonArray = new JSONArray(jsonResponse);
+
+            users = new ArrayList<>();
+
+            String id, name, imageUrl;
+
+            for (int i = 0; i < jsonArray.length(); i++){
+                id = jsonArray.getJSONObject(i).getString("id");
+                name = jsonArray.getJSONObject(i).getString("name");
+                imageUrl = jsonArray.getJSONObject(i).getString("img");
+
+                users.add(new User(id, name, imageUrl));
+
+            }
+
+            Log.d(LOG_TAG, "    FETCH EARTHQUAKE DATA, QueryUtils");
+            return users;
+
+        } catch (JSONException e){
+            return null;
+        }
+
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with creating URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    public static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+}
---
+++ app/src/main/java/com/lacourt/picpay/picpaymobiledev/User.java
@@ -0,0 +1,41 @@
+package com.lacourt.picpay.picpaymobiledev;
+
+/**
+ * Created by igor on 17/08/2017.
+ */
+
+public class User {
+    private String id;
+    private String name;
+    private String imageUrl;
+
+    public User(String id, String name, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
