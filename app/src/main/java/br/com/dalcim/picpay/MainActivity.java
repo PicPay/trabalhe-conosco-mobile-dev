512c1bdbcdc2fd0b0235a23aabbec9fb3abd6dd7
wiliamtrancoso <wileleh@gmail.com>
2017-08-24 23:07:13 -0300

    initial commit

---
+++ app/src/main/java/br/com/dalcim/picpay/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.dalcim.picpay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
