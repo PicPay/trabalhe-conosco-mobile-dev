bdb45a1d37fd3d003bcb2cd8c3fded42b2bda570
E. Cardoso de Araújo <everaldocardosodearaujo@gmail.com>
2018-03-14 21:46:12 -0300

    Criando Layout

---
+++ PicPay/app/src/main/java/br/com/everaldocardosodearaujo/picpay/ContactActivity.java
@@ -0,0 +1,13 @@
+package br.com.everaldocardosodearaujo.picpay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
--- PicPay/app/src/main/java/br/com/everaldocardosodearaujo/picpay/SplashActivity.java
+++ PicPay/app/src/main/java/br/com/everaldocardosodearaujo/picpay/SplashActivity.java
@@ -1,7 +1,9 @@
 package br.com.everaldocardosodearaujo.picpay;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +11,16 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent it;
+                it = new Intent(SplashActivity.this,ContactActivity.class);
+
+                SplashActivity.this.startActivity(it);
+                SplashActivity.this.finish();
+            }
+        }, 4000);
     }
 }
