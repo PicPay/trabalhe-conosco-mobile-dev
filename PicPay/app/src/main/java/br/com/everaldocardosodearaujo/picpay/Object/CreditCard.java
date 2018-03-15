c1a9b4e2d3c5b5c04201b068819463c3823ccc2c
E. Cardoso de Araújo <everaldocardosodearaujo@gmail.com>
2018-03-15 00:25:29 -0300

    Criando Layout

---
+++ PicPay/app/src/main/java/br/com/everaldocardosodearaujo/picpay/Object/CreditCard.java
@@ -0,0 +1,80 @@
+package br.com.everaldocardosodearaujo.picpay.Object;
+
+/**
+ * Created by E. Cardoso de Araújo on 15/03/2018.
+ */
+
+public class CreditCard {
+    private long id;
+    private String flag;
+    private String name;
+    private String numberCard;
+    private String validity;
+    private String ccv;
+
+    public CreditCard(){
+        this.id = 0;
+        this.flag = "";
+        this.name = "";
+        this.numberCard = "";
+        this.validity = "";
+        this.ccv = "";
+    }
+
+    public CreditCard(long id,String flag, String name, String numberCard, String validity, String ccv){
+        this.id = id;
+        this.flag = flag;
+        this.name = name;
+        this.numberCard = numberCard;
+        this.validity = validity;
+        this.ccv = ccv;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumberCard() {
+        return numberCard;
+    }
+
+    public void setNumberCard(String numberCard) {
+        this.numberCard = numberCard;
+    }
+
+    public String getValidity() {
+        return validity;
+    }
+
+    public void setValidity(String validity) {
+        this.validity = validity;
+    }
+
+    public String getCcv() {
+        return ccv;
+    }
+
+    public void setCcv(String ccv) {
+        this.ccv = ccv;
+    }
+}
