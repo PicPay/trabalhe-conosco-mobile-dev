bb317557d716b925a9619164913077358688be96
Lucas Romano <lucasromanomr@gmail.com>
2018-04-22 11:57:35 -0300

    Init

---
+++ picpay/picpayTests/picpayTests.swift
@@ -0,0 +1,36 @@
+//
+//  picpayTests.swift
+//  picpayTests
+//
+//  Created by Lucas Romano Marquez Rizzi on 22/04/2018.
+//  Copyright © 2018 Lucas Romano Marquez Rizzi. All rights reserved.
+//
+
+import XCTest
+@testable import picpay
+
+class picpayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
