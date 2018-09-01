b1f159e929964241ca0254558c4134d41bbe0bc1
FaustFelipe <felipefaustini23@outlook.com>
2018-08-31 23:30:55 -0300

    Initial commit

---
+++ app/src/main/java/projetosestudos/felipe/com/trabalhe_conosco_mobile_dev/MainActivity.kt
@@ -0,0 +1,12 @@
+package projetosestudos.felipe.com.trabalhe_conosco_mobile_dev
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
