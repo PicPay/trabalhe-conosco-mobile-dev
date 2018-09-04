6fb7520e1009fc4f389f9e7702f58bbd087d4c36
david.rocha <davidrocha@outlook.com>
2018-09-04 14:39:23 -0300

    rodando

--- app/src/main/java/com/picpay/david/davidrockpicpay/features/UsersList/ListUsersActivity.kt
+++ app/src/main/java/com/picpay/david/davidrockpicpay/features/UsersList/ListUsersActivity.kt
@@ -3,11 +3,23 @@ package com.picpay.david.davidrockpicpay.features.UsersList
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.picpay.david.davidrockpicpay.R
+import com.picpay.david.davidrockpicpay.features.base.BaseActivity
+import com.picpay.david.davidrockpicpay.models.User
 
-class ListUsersActivity : AppCompatActivity() {
+class ListUsersActivity : BaseActivity(), ListUsersMvpView {
+
+    override fun getAllUsers() {
+        super.showMessage("Ok TOAST!")
+    }
+
+    override fun fillList(users: List<User>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_users)
+
+        getAllUsers()
     }
 }
