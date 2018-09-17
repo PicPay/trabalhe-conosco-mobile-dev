09c587fd1ad2a2064cef767555f6b249db021c18
Rapahel Carletti <carlettiraphael@gmail.com>
2018-09-17 15:49:00 -0300

    Create project

---
+++ CardRequisition/CardRequisition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardRequisition
+//
+//  Created by Raphael Carletti on 9/17/18.
+//  Copyright © 2018 Raphael Carletti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
