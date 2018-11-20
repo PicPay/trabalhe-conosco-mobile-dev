81051ba2ad32f1f224103672438c4b65764fa275
Paulo Eduardo Santos de Sousa <eduardo.lwx@gmail.com>
2018-11-20 15:29:57 -0300

    Add domain module

---
+++ domain/src/main/java/com/eduardo/trabalheconoscomobiledev/domain/interactor/CompletableUseCase.kt
@@ -0,0 +1,31 @@
+package com.eduardo.trabalheconoscomobiledev.domain.interactor
+
+import com.eduardo.trabalheconoscomobiledev.domain.executor.PostExecutionThread
+import io.reactivex.Completable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableCompletableObserver
+import io.reactivex.schedulers.Schedulers
+
+abstract class CompletableUseCase<in Params> (private val postExecutionThread: PostExecutionThread) {
+
+    private val disposables = CompositeDisposable()
+
+    protected abstract fun buildUseCaseCompletable(params: Params? = null): Completable
+
+    open fun execute(observer: DisposableCompletableObserver, params: Params? = null) {
+        val completable = this.buildUseCaseCompletable(params)
+            .subscribeOn(Schedulers.io())
+            .observeOn(postExecutionThread.scheduler)
+        addDisposable(completable.subscribeWith(observer))
+    }
+
+    fun addDisposable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+
+    fun dispose() {
+        if (!disposables.isDisposed) disposables.dispose()
+    }
+
+}
---
+++ domain/src/test/java/com/eduardo/trabalheconoscomobiledev/domain/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package com.eduardo.trabalheconoscomobiledev.domain
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect2() {
+        assertEquals(4, 2 + 2)
+    }
+}
