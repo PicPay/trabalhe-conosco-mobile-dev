60d2ab16f0a46a9d14f7731e7e71bb0bc4a9f819
Paulo Eduardo Santos de Sousa <eduardo.lwx@gmail.com>
2018-11-20 13:11:21 -0300

    Migrate to AndroidX

--- app/src/androidTest/java/com/eduardo/trabalheconoscomobiledev/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/eduardo/trabalheconoscomobiledev/ExampleInstrumentedTest.kt
@@ -1,12 +1,12 @@
 package com.eduardo.trabalheconoscomobiledev
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,7 +18,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext: Context = ApplicationProvider.getApplicationContext()
         assertEquals("com.eduardo.trabalheconoscomobiledev", appContext.packageName)
     }
 }
