38227f540f1499f821c23f6d147dd917e43c7c48
Edney O. S. Filho <edney.osf@gmail.com>
2018-12-19 00:08:21 -0200

    upload

--- app/src/main/java/test/edney/picpay/view/card/fragment/CardRegisterFragment.kt
+++ app/src/main/java/test/edney/picpay/view/card/fragment/CardRegisterFragment.kt
@@ -148,7 +148,7 @@ class CardRegisterFragment : Fragment() {
                         canRegister.value = checkArray
                   }
             })
-            binding.tvCvv.addTextChangedListener(object : TextWatcher{
+            binding.tvCvv.addTextChangedListener(object : TextWatcher {
                   override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
                   override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
                   override fun afterTextChanged(s: Editable?) {
--- app/src/main/java/test/edney/picpay/view/card/fragment/CardRegisterUI.kt
+++ app/src/main/java/test/edney/picpay/view/card/fragment/CardRegisterUI.kt
@@ -1,5 +1,5 @@
 package test.edney.picpay.view.card.fragment
 
 interface CardRegisterUI {
-    fun actionSave()
+      fun actionSave()
 }
