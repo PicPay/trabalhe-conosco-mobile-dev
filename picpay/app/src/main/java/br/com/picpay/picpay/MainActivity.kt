4c1a1784c489db1e90887383c9c23df6775b0a4f
Valmir Torres <valmirjr_20@hotmail.com>
2018-12-21 10:59:02 -0200

    contact_list

--- picpay/app/src/main/java/br/com/picpay/picpay/MainActivity.kt
+++ picpay/app/src/main/java/br/com/picpay/picpay/MainActivity.kt
@@ -2,11 +2,13 @@ package br.com.picpay.picpay
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_contacts.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_contacts)
+        setSupportActionBar(coordinator_toolbar)
     }
 }
