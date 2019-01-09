57ab66875b56824ea01c0b806423ec38be008898
Rodolfo Gusson <rodolfoggp@gmail.com>
2019-01-09 17:53:07 -0200

    Initial implementation of network classes

---
+++ app/src/main/java/rodolfogusson/testepicpay/data/UserRepository.kt
@@ -0,0 +1,11 @@
+package rodolfogusson.testepicpay.data
+
+import rodolfogusson.testepicpay.network.SendMoneyService
+
+class UserRepository {
+    private val service = SendMoneyService.create()
+//
+//    fun getUsersList() {
+//        service.getUsersList().enqueue()
+//    }
+}
