8c0a4ed943582e3001f8dfaa8f61a5b34dc8631c
Rodolfo Gusson <rodolfoggp@gmail.com>
2019-01-25 19:47:45 -0200

    Adjusted ContactListViewModel and receipt layout
    - ContactListViewModel now updates data inside the receipt layout.

--- app/src/main/java/rodolfogusson/testepicpay/sendmoney/viewmodel/contact/ContactListViewModel.kt
+++ app/src/main/java/rodolfogusson/testepicpay/sendmoney/viewmodel/contact/ContactListViewModel.kt
@@ -1,15 +1,17 @@
 package rodolfogusson.testepicpay.sendmoney.viewmodel.contact
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.*
 import rodolfogusson.testepicpay.sendmoney.model.contact.Contact
 import rodolfogusson.testepicpay.sendmoney.model.contact.ContactRepository
 import rodolfogusson.testepicpay.core.network.Resource
 import rodolfogusson.testepicpay.sendmoney.model.creditcard.CreditCard
 import rodolfogusson.testepicpay.sendmoney.model.creditcard.CreditCardRepository
 import rodolfogusson.testepicpay.sendmoney.model.payment.Transaction
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class ContactListViewModel(
     application: Application,
@@ -22,13 +24,33 @@ class ContactListViewModel(
 
     val transactionCompleted = MutableLiveData<Transaction>()
 
+    val transactionDate = MediatorLiveData<String>()
+
+    val transactionTime = MediatorLiveData<String>()
+
     private val creditCardRepository = CreditCardRepository.getInstance(application)
 
     private val contactRepository = ContactRepository()
 
+    private lateinit var dateTime: LocalDateTime
+
     init {
         contacts = contactRepository.getContacts()
         lastRegisteredCard = creditCardRepository.getLastRegisteredCreditCard()
-        transaction?.let { transactionCompleted.value = it }
+
+        transactionDate.addSource(transactionCompleted) {
+            val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            transactionDate.setValue(dateTime.format(formatter))
+        }
+
+        transactionTime.addSource(transactionCompleted) {
+            val formatter = DateTimeFormatter.ofPattern("HH:mm")
+            transactionTime.setValue(dateTime.format(formatter))
+        }
+
+        transaction?.let {
+            dateTime = Instant.ofEpochSecond(it.timestamp).atZone(ZoneId.systemDefault()).toLocalDateTime()
+            transactionCompleted.value = it
+        }
     }
 }
